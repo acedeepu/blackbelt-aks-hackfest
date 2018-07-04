102f06398d39189940d265f06027b44a255408dc
acedeepu <40825064+acedeepu@users.noreply.github.com>
2018-07-04 16:20:37 +0530

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team Test -1 
+        Azure Global Blackbelt Team Test -2
       </div>
       <div class="col-lg-6">
       </div>
